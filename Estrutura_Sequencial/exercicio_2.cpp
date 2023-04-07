2009eeddb85a304373f2b74e06015228a7f9ef95
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2023-04-07 16:15:32 -0300

    exercicio_2

---
+++ Estrutura_Sequencial/exercicio_2.cpp
@@ -0,0 +1,17 @@
+//Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    float num = 0;
+
+    cout << "Digite um número" << endl;
+    cin >> num;
+
+    cout <<"O número informado é: " <<num;
+
+    return 0;
+
+}
