4ae4606e8b38aeb27bd13723c81115d0f12b688a
Thiago Silva Alves <thiagosilvaalvesmusic@gmail.com>
2024-01-23 10:17:24 -0300

    Update exercicio_016.cpp

--- Vetor/exercicio_016.cpp
+++ Vetor/exercicio_016.cpp
@@ -22,7 +22,51 @@ Desafio: Crie ma fórmula para chegar na posição da lista a partir do salário
 
 #include <iostream>
 
+using namespace std;
+
 int main(){
 
-     
+    float salario_fixo = 200;
+    float salario_extra = 0;
+    float salario_bruto = 0;
+    float valor_vendas = 0;
+    float percentual = 0.09;
+    int quant_vendedor = 30;
+    int posicao = 0;
+    int contador[9] = {0};
+
+    cout <<"Informe a venda bruda de cada vendedor e digite -1 para finalizar."<<endl;
+
+    for (int i = 0; i < quant_vendedor; i++){
+        
+        cout <<"Informe o valor das vendas do vendedor "<< i + 1 <<": ";
+        cin >>valor_vendas;
+
+        if (valor_vendas == -1){
+            break;
+        }
+
+        salario_extra = valor_vendas * percentual;
+        salario_bruto = salario_extra + salario_fixo;
+        posicao = (salario_bruto - salario_fixo)/100;
+
+        if (posicao > 8){
+            
+            posicao = 8;
+        }
+        
+        contador[posicao]++;
+    }
+    
+    cout <<"Quantidade de vendedores quereceberam  de $200 - $299 :" <<contador[0] <<endl;
+    cout <<"Quantidade de vendedores quereceberam  de $300 - $399 :" <<contador[1] <<endl;
+    cout <<"Quantidade de vendedores quereceberam  de $400 - $499 :" <<contador[2] <<endl;
+    cout <<"Quantidade de vendedores quereceberam  de $500 - $599 :" <<contador[3] <<endl;
+    cout <<"Quantidade de vendedores quereceberam  de $600 - $699 :" <<contador[4] <<endl;
+    cout <<"Quantidade de vendedores quereceberam  de $700 - $799 :" <<contador[5] <<endl;
+    cout <<"Quantidade de vendedores quereceberam  de $800 - $899 :" <<contador[6] <<endl;
+    cout <<"Quantidade de vendedores quereceberam  de $900 - $999 :" <<contador[7] <<endl;
+    cout <<"Quantidade de vendedores quereceberam  de $1000 em diante :" <<contador[8] <<endl;
+
+    return 0;
 }
